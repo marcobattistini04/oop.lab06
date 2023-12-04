a5f1cc862251ce78620b958abb69c957283dcbed
RicciNicholas <nicholas.ricci3@unibo.it>
2023-12-04 14:30:18 +0100

    Completed 62

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,13 +1,28 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
  *
  */
 public final class UseListsAndMaps {
+
+    private static final int START = 1_000;
+    private static final int END = 2_000;
+    private static final int ELEMS = 100_000;
+    private static final int READS = 1_000;
+    private static final long AFRICAN_POPULATION = 1_110_635_000L;
+    private static final long AMERICAS_POPULATION = 972_005_000L;
+    private static final long ANTARTICA_POPULATION = 0L;
+    private static final long ASIA_POPULATION = 4_298_723_000L;
+    private static final long EUROPE_POPULATION = 742_752_000L;
+    private static final long OCEANIA_POPULATION = 38_304_000L;    
 
     private UseListsAndMaps() {
     }
@@ -21,19 +36,39 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        final List<Integer> arrayList = new ArrayList<>();
+        for (int i = START; i <= END; i++) {
+            arrayList.add(i);
+        }
+        System.out.println(arrayList);
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        final List<Integer> linkedList = new LinkedList<>(arrayList);
+        System.out.println(linkedList);
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        int tmp = arrayList.get(0);
+        arrayList.set(0, arrayList.get(arrayList.size() - 1));
+        arrayList.set(arrayList.size() - 1, tmp);
+        System.out.println(linkedList);
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        final var builder = new StringBuilder();
+        for (final int i : arrayList) {
+            builder.append(i);
+            builder.append(", ");
+        }
+        if (builder.length() > 0) {
+            builder.delete(builder.length() - 2, builder.length());
+        }
+        System.out.println("[" + builder + "]");
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +76,42 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+        long time = System.nanoTime();
+        for (int i = 1; i <= ELEMS; i++) {
+            arrayList.add(0, i);
+        }
+        time = System.nanoTime() - time;
+        var millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("Inserting " + ELEMS + " elements as first element of the array list took " + time + "ns (" + millis + "ms)");
+
+        time = System.nanoTime();
+        for (int i = 1; i <= ELEMS; i++) {
+            linkedList.add(0, i);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("Inserting " + ELEMS + " elements as first element of the linked list took " + time + "ns (" + millis + "ms)");
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
          */
+        time = System.nanoTime();
+        for (int i = 0; i < READS; i++) {
+            var read = arrayList.get(arrayList.size() / 2);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("Reading " + READS + " times an elements whose poition is in the middle of the array list took " + time + "ns (" + millis + "ms)");
+
+        time = System.nanoTime();
+        for (int i = 0; i < READS; i++) {
+            var read = linkedList.get(linkedList.size() / 2);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println("Reading " + READS + " times an elements whose poition is in the middle of the linked list took " + time + "ns (" + millis + "ms)");
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
@@ -63,8 +128,20 @@ public final class UseListsAndMaps {
          *
          * Oceania -> 38,304,000
          */
+        final Map<String, Long> world = new HashMap<>();
+        world.put("Africa", AFRICAN_POPULATION);
+        world.put("Americas", AMERICAS_POPULATION);
+        world.put("Antartica", ANTARTICA_POPULATION);
+        world.put("Asia", ASIA_POPULATION);
+        world.put("Europe", EUROPE_POPULATION);
+        world.put("Oceania", OCEANIA_POPULATION);
         /*
          * 8) Compute the population of the world
          */
+        long worldPopulation = 0;
+        for (final long population : world.values()) {
+            worldPopulation = worldPopulation + population;
+        }
+        System.out.println("World population: " + worldPopulation);
     }
 }
